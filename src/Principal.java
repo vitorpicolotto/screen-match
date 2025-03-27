d74af2e567c2294ef26607b6ba840b68f7ed7c51
vitorpicolotto <picolottovitor@gmail.com>
2025-03-26 23:11:30 -0300

    criando pacotes e acessando via getters e setters

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,11 @@
+import screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nomeFilme = "Harry Potter e a Pedra Filosofal";
-        meuFilme.anoDeLancamento = 2001;
-        meuFilme.duracaoEmMinutos = 142;
+        meuFilme.setNomeFilme("Harry Potter e a Pedra Filosofal");
+        meuFilme.setAnoDeLancamento(2001);
+        meuFilme.setDuracaoEmMinutos(142);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
---
+++ src/screenmatch/modelos/Filme.java
@@ -0,0 +1,64 @@
+package screenmatch.modelos;
+
+public class Filme {
+    //atributos guardados dentro do objeto (encapsulamento?)
+    private String nomeFilme;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes; //modificador de acesso - private restringe que o atributo seja modificado
+    private int totalAvaliacoes;
+    private int duracaoEmMinutos;
+
+    //Metodos: função dentro da classe
+
+    public int getTotalAvaliacoes(){
+        return totalAvaliacoes;
+    } //metodo acessor - para devolver o valor de um atributo privado
+
+    public String getNomeFilme() {
+        return nomeFilme;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setNomeFilme(String nomeFilme) {
+        this.nomeFilme = nomeFilme;
+    } //alt+isert Setter; this -referencia que indica o valor de um filme no objeto ATUAL, não ao parâmetro do método
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void exibeFichaTecnica(){
+        System.out.println("Título: " + nomeFilme);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+    }
+
+    public void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalAvaliacoes++;
+    }
+
+    public double pegaMedia(){
+        return somaDasAvaliacoes / totalAvaliacoes;
+    }
+
+}
